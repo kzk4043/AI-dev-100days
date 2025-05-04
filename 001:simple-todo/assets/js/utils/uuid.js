c0ce3512b15cffa15ad1b1968dc4d2d9f6b48f8c
kzk <50351164+kzk4043@users.noreply.github.com>
2025-05-05 00:35:10 +0900

    Add uuid.js utility

---
+++ 001:simple-todo/assets/js/utils/uuid.js
@@ -0,0 +1,12 @@
+/**
+ * Generate a simple UUID (v4 format)
+ * Not as robust as a dedicated library but sufficient for this application
+ * @returns {string} A UUID v4 string
+ */
+function generateUUID() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    const r = Math.random() * 16 | 0;
+    const v = c === 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
+}
